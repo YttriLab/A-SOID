48e8de8dbfb1dc5b73b48a8aced056193913b79f
JensBlack <j.schweihoff@gmail.com>
2023-01-05 15:37:18 +0100

    fixed "other" being added as empty column due to reindex

--- asoid/utils/load_preprocess.py
+++ asoid/utils/load_preprocess.py
@@ -385,11 +385,6 @@ class Preprocess:
         if "other" not in self.classes and not self.exclude_other:
             self.classes.append("other")
 
-        #get rid of "other" column if excluded
-        elif self.exclude_other:
-            select_label_df.drop(columns=["other"], inplace=True)
-
-
         # make sure that all classes are in (irrelevant for most cases):
         for selected_class in self.classes:
             if selected_class not in select_label_df.columns:
@@ -399,6 +394,11 @@ class Preprocess:
         # rearrange so that "other" is always last and classes are in the order they were selected:
         select_label_df = select_label_df.reindex(
             columns=(list([clm for clm in self.classes if clm != "other"]) + ["other"]))
+
+        #get rid of "other" column if excluded
+        if self.exclude_other:
+            select_label_df.drop(columns=["other"], inplace=True)
+
         # convert dummie encoding to numbers but keep also identity of classes not represented in this file
         label_vector = np.argmax(np.array(select_label_df), axis=1)
 
