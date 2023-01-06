11283a74ba1296fe174d11ec7acad0c8a996f6a6
JensBlack <j.schweihoff@gmail.com>
2023-01-06 11:58:20 +0100

    raised version to reflect new changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='asoid',
-    version='0.1.1',
+    version='0.2.0',
     description='ASOiD: An active learning approach to behavioral classification',
     long_description=long_description,
     long_description_content_type="text/markdown",
