38bd77247959484c29c475878389ae7d4aacf503
JensBlack <j.schweihoff@gmail.com>
2023-04-04 13:03:42 +0200

    restructured calculation, saving, and loading of motion energy to split by behavior. Should avoid giant files and long loading times.

--- asoid/utils/motionenergy.py
+++ asoid/utils/motionenergy.py
@@ -300,6 +300,57 @@ class MotionEnergyMachine:
                         #animate and save file
                         animate_blobs(class_data_ego, video_name, outlines=outline_dict)
 
+    def extract_frames_single(self, selected_behavior):
+        video_import_path = os.path.join(self.working_dir, self.prefix, "animations")
+        info_box = st.empty()
+        # frame_list = {}
+        with st.spinner("Extracting frames from videos for actions"):
+
+            files = glob.glob(str.join('', (os.path.join(video_import_path, selected_behavior), '/*.avi')),
+                              recursive=True)
+            if not files:
+                raise FileNotFoundError("No videos found in folder. Please run the animation step first.")
+
+            behavior_i = {}
+            for idx in stqdm(range(len(files)), desc="Going through examples..."):
+                curr_video = files[idx]
+                curr_filename = os.path.basename(curr_video)[:-4]
+                cap = cv2.VideoCapture(curr_video)
+                frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+                resize_f = 3
+                resize_dim = (frameHeight // resize_f, frameWidth // resize_f)
+
+                # convert video into numpy array of frames (binary)
+
+                success, image = cap.read()
+                count = 0
+                frame_array = np.empty((frameCount, resize_dim[0], resize_dim[1]), np.dtype('uint8'))
+                while count < frameCount - 1 and success:
+                    success, img = cap.read()
+                    # convert to grayscale
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    # convert to binary
+                    thresh = 1
+                    im_bw = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)[1]
+                    # resize for easier calculations
+                    res_bw = cv2.resize(im_bw, (resize_dim[0], resize_dim[1]))
+                    # add to numpy array
+                    frame_array[count] = res_bw
+                    count += 1
+                behavior_i[curr_filename] = frame_array
+                # frame_list[selected_behavior] = behavior_i
+                cap.release()
+
+            # save it for next time
+            frames_collection_path = os.path.join(self.working_dir, self.prefix, "animations", selected_behavior,
+                                                  "f_collection_{}".format(selected_behavior))
+            with open(frames_collection_path, 'wb') as f:
+                joblib.dump(behavior_i, f)
+
+
     def extract_frames(self):
         video_import_path = os.path.join(self.working_dir, self.prefix, "animations")
         info_box = st.empty()
@@ -358,7 +409,16 @@ class MotionEnergyMachine:
 
         return temp_frames
 
-    def calc_motion_energy(self):
+    def calc_motion_energy_single(self, selected_behavior):
+        """Calculates motion energy for a single behavior and saves it to disk"""
+        frames = self.load_frames_single(selected_behavior)
+        motion_energy = calc_motion_energy_single(frames)
+        self.save_motion_energy_single(motion_energy, selected_behavior)
+        return motion_energy
+
+    def calc_motion_energy_all(self):
+        """Calculates motion energy for all behaviors and saves them seperately to disk
+        :return: dictionary with motion energy for each behavior"""
         motion_energy_by_behavior = {}
         for i in stqdm(range(len(self.annotation_classes)), desc = "Calculating motion energy"):
             selected_behavior = self.annotation_classes[i]
@@ -367,6 +427,7 @@ class MotionEnergyMachine:
             except FileNotFoundError:
                 continue
             motion_energy = calc_motion_energy_single(frames)
+            self.save_motion_energy_single(motion_energy, selected_behavior)
             motion_energy_by_behavior[selected_behavior] = motion_energy
 
         if not motion_energy_by_behavior:
@@ -375,77 +436,96 @@ class MotionEnergyMachine:
 
         return motion_energy_by_behavior
 
-    def view_motion_energy(self, motion_energy, view_selection = "Across sessions"):
+    def save_motion_energy_single(self, motion_energy, selected_behavior):
+        """Saves motion energy for a single behavior"""
+        # save it for next time by behavior
+        motion_energy_path = os.path.join(self.working_dir, self.prefix, "animations", selected_behavior, "motion_energy.sav")
+        with open(motion_energy_path, 'wb') as f:
+            joblib.dump(motion_energy, f)
 
+    def save_motion_energy_all(self, motion_energy):
+        """Saves motion energy for all behaviors seperated by behavior"""
+        # save it for next time by behavior
+        for key in motion_energy.keys():
+            motion_energy_path = os.path.join(self.working_dir, self.prefix, "animations", key, "motion_energy.sav")
+            with open(motion_energy_path, 'wb') as f:
+                joblib.dump(motion_energy[key], f)
+
+    @st.cache(allow_output_mutation=True)
+    def load_motion_energy_single(self, selected_behavior):
+        """Loads motion energy for a single behavior"""
+        motion_energy_path = os.path.join(self.working_dir, self.prefix, "animations", selected_behavior, "motion_energy.sav")
+        with open(motion_energy_path, 'rb') as fr:
+            temp_motion_energy = joblib.load(fr)
+
+        return temp_motion_energy
+
+    def load_motion_energy_all(self):
+        """Loads motion energy for all behaviors"""
+        motion_energy_by_behavior = {}
+        for i in range(len(self.annotation_classes)):
+            selected_behavior = self.annotation_classes[i]
+            try:
+                motion_energy = self.load_motion_energy_single(selected_behavior)
+            except FileNotFoundError:
+                continue
+            motion_energy_by_behavior[selected_behavior] = motion_energy
+
+        if not motion_energy_by_behavior:
+            #if it is empty because it did not find any animations
+            raise FileNotFoundError
+
+        return motion_energy_by_behavior
+
+    def view_motion_energy(self, motion_energy):
+        """Visualizes motion energy for all behaviors in motion_energy dictionary"""
         #TODO UI adjustment of color range
         #c_range = st.slider("Color range", min_value = 0 , max_value = 100, [0, 20])
         c_range = [0, 20]
         motion_energy_keys = list(motion_energy.keys())
-        fig = make_subplots(rows=1, cols=len(motion_energy_keys)
-                            , subplot_titles= motion_energy_keys
+
+        n_cols = 5
+        n_rows = int(np.ceil(len(motion_energy_keys)/n_cols))
+
+
+        fig = make_subplots(rows=n_rows, cols=n_cols
+                            , subplot_titles=motion_energy_keys
+                            #, shared_xaxes=True, shared_yaxes=True
+                            , column_widths=[1] * n_cols, row_heights=[1] * n_rows
+                            , horizontal_spacing=0.05, vertical_spacing=0.1
                             )
+        for i, action_type in enumerate(motion_energy_keys):
+            #get all bouts per behavior
+            all_bouts = list(motion_energy[action_type].values())
+            #show the average motion energy across all bouts per behavior
+            avg_motion_energy = np.nanmean(all_bouts, axis=0)
+            fig.add_trace(go.Heatmap(z= avg_motion_energy
+                                     ,name=action_type
+                                     , showscale=False
+                                     ,zmin=c_range[0]
+                                     ,zmax=c_range[1]
+                                     ,colorscale= px.colors.sequential.Viridis
+                                     )
+                          ,col = i % n_cols + 1
+                          ,row = i // n_cols + 1)
 
-        if view_selection == "Across sessions":
-            for i, action_type in enumerate(motion_energy_keys):
-                #get all bouts per behavior
-                all_bouts = list(motion_energy[action_type].values())
-                #show the average motion energy across all bouts per behavior
-                avg_motion_energy = np.nanmean(all_bouts, axis=0)
-                fig.add_trace(go.Heatmap(z= avg_motion_energy
-                                         ,name=action_type
-                                         , showscale=True
-                                         #colorbar=dict(title='Intensity',
-                                         #              x=1.05, len=0.5),
-                                         ,zmin=c_range[0]
-                                         ,zmax=c_range[1]
-                                         ,colorscale= px.colors.sequential.Viridis
-                                         )
-                              ,col = i +1
-                              ,row = 1)
-        elif view_selection == "Per session":
-            for i, action_type in enumerate(motion_energy_keys):
-                # get all bouts per behavior
-                #all_bouts = list(motion_energy[action_type].values())
-                # todo: add functionality to do per session per behavior
-                #get keys with same starting string (same session name)
-                all_bout_names = motion_energy[action_type].keys()
-                all_session_names = [x.split("_example")[0] for x in all_bout_names]
-                all_session_names = np.unique(all_session_names)
+        fig.update_layout(title='Motion energy'
+                          )
 
-                for bout_name, bout_me in motion_energy[action_type].items():
-                    # show the average motion energy across all bouts per behavior
-                    avg_motion_energy = np.nanmean(all_bouts, axis=0)
-                    fig.add_trace(go.Heatmap(z=avg_motion_energy
-                                             , name=action_type
-                                             , showscale=True
-                                             # colorbar=dict(title='Intensity',
-                                             #              x=1.05, len=0.5),
-                                             , zmin=c_range[0]
-                                             , zmax=c_range[1]
-                                             , colorscale=px.colors.sequential.Viridis
-                                             )
-                                  , col=i + 1
-                                  , row=1)
-        else:
-            raise ValueError("Invalid view selection: {}".format(view_selection))
+        fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False)
+        fig.update_yaxes(showticklabels=False, showgrid=False, zeroline=False)
 
-
-
-        fig.update_layout(title='Motion energy')
-        fig.update_xaxes(showticklabels = False)
-        fig.update_yaxes(showticklabels=False)
-        #fig.update(layout_coloraxis_showscale=False)
-
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True, height= 600*n_rows)
 
     def get_motionenergy(self):
 
-        param_exp = st.expander("Generate animations", expanded= True)
+        param_container  = st.container()
         motion_container = st.container()
 
         #get user input for egocentric alignment
         #TODO: limit selection to two
-        with param_exp:
+        with param_container:
+            st.text("Generate animations")
             ego_container = st.container()
             polygon_container = st.container()
             animation_info_box = st.empty()
@@ -475,36 +555,46 @@ class MotionEnergyMachine:
 
 
         with motion_container:
-            motion_info_box = st.empty()
             #for later
-            motion_energy = None
+            motion_energy = dict()
+            selected_me_behaviors = st.multiselect("Select behavioral classes for motion energy view",
+                                                   self.annotation_classes, self.annotation_classes)
+            motion_info_box = st.empty()
 
-            try:
-                # try to load the file if it already exists
-                motion_energy = load_motion_energy(self.working_dir, self.prefix)
-                view_selection = st.selectbox("Motion Energy view:", ("Across sessions", "Per session"))
-                if st.button("View Motion Energy", key="me_viewer"):
-                    self.view_motion_energy(motion_energy, view_selection)
-
-            except FileNotFoundError:
-                if st.button("Calculate Motion Energy"):
-                    motion_info_box = st.info("This may take a long time...")
+            #button to load motion energy
+            if st.button("Load & view Motion Energy", key="me_viewer"):
+                #load motion energy based on selected behaviors
+                for i in stqdm(range(len(selected_me_behaviors)), desc = "Collecting behaviors..."):
+                    behavior = selected_me_behaviors[i]
                     try:
-                        motion_energy = self.calc_motion_energy()
-                        save_data(self.working_dir, self.prefix, "motionenergy.sav", motion_energy)
+                        # load the motion energy
+                        with st.spinner("Loading motion energy..."):
+                            motion_energy[behavior] = self.load_motion_energy_single(behavior)
                     except FileNotFoundError:
-                        # extract frames
+                        motion_info_box.warning("Motion energy for {} not found. Calculating motion energy...".format(behavior))
                         try:
-                            self.extract_frames()
-                            motion_energy = self.calc_motion_energy()
-                            save_data(self.working_dir, self.prefix, "motionenergy.sav", motion_energy)
+                            # if the file does not exist, calculate it
+                            with st.spinner("Calculating motion energy..."):
+                                motion_energy[behavior] = self.calc_motion_energy_single(behavior)
                         except FileNotFoundError:
-                            motion_info_box.error("Generate animations first.")
+                            # if the file does not exist, extract the frames and calculate it
+                            try:
+                                with st.spinner("Extracting frames..."):
+                                    self.extract_frames_single(behavior)
+                                with st.spinner("Calculating motion energy..."):
+                                    motion_energy[behavior] = self.calc_motion_energy_single(behavior)
+                            except FileNotFoundError:
+                                # if nothing helps, skip the behavior
+                                motion_info_box.warning("Frames for {} not found. Generate animations first. Skipping behavior.".format(behavior))
+                                continue
 
-                if motion_energy is not None:
-                    view_selection = st.selectbox("Motion Energy view:", ("Across sessions", "Per session"))
-                    if st.button("View Motion Energy", key= "me_viewer"):
-                        self.view_motion_energy(motion_energy, view_selection)
+                # if the dictionary is not empty, view it
+                if motion_energy:
+                    #view motion energy
+                    motion_info_box.info("Motion energy loaded. Viewing motion energy...")
+                    self.view_motion_energy(motion_energy)
+                else:
+                    motion_info_box.error("No motion energy found. Generate animations first.")
 
 
     def main(self):
