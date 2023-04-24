14ca15611c960dc8c8a6e466fbb6d6c11d39d60d
JensBlack <j.schweihoff@gmail.com>
2023-04-24 16:19:05 +0200

    added __version__ to init.py for one-version solution added get_requirements() to setup.py for one-version solution

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 from os import path
+import asoid
+
+
 curr_dir = path.abspath(path.dirname(__file__))
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def get_requirements():
+    with open(path.join(curr_dir, "requirements.txt"), encoding="utf-8") as f:
+        return f.read().strip().split("\n")
 
 setup(
     name='asoid',
-    version='0.3',
+    version= asoid.__version__,
     description='ASOiD: An active learning approach to behavioral classification',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,31 +31,6 @@ setup(
         },
     packages=find_packages(),  # same as name
     include_package_data=True,
-    install_requires=["matplotlib"
-                    ,"numpy"
-                    ,"pandas"
-                    ,"seaborn"
-                    ,"streamlit>=1.20.0"
-                    ,"streamlit_option_menu"
-                    ,"hydralit"
-                    ,"opencv-python"
-                    ,"tqdm"
-                    ,"stqdm"
-                    ,"scikit-learn<1.3.0"
-                    ,"h5py"
-                    ,"plotly"
-                    ,"pillow"
-                    ,"joblib==1.3.2"
-                    ,"scipy"
-                    ,"ipython"
-                    ,"psutil"
-                    ,"numba"
-                    ,"hdbscan==0.8.33"
-                    ,"setuptools"
-                    ,"umap-learn"
-                    ,"click"
-                    ,"moviepy"
-                    ,"ffmpeg-python"
-                    ],
+    install_requires=get_requirements(),
     python_requires=">=3.7",
 )
