f73463972153809626645c3020fe330cb435d217
JensBlack <j.schweihoff@gmail.com>
2023-05-11 17:20:06 +0200

    added cli option to predict

--- asoid/__main__.py
+++ asoid/__main__.py
@@ -2,6 +2,13 @@ import click
 import os
 import configparser as cfg
 
+import sys
+# get current dir
+dirname = os.path.dirname(__file__)
+sys.path.append(dirname)
+
+from headless import Project
+
 def load_streamlit_config(dirname):
     config = cfg.ConfigParser()
     #make config case sensitive
@@ -26,6 +33,7 @@ def main():
 
 @main.command("app")
 def main_streamlit():
+    """Runs the A-SOiD streamlit app"""
     #get current dir
     dirname = os.path.dirname(__file__)
 
@@ -38,6 +46,30 @@ def main_streamlit():
     # run streamlit app using the hideous way and pass args
     # because streamlit has no native support for this
     os.system('streamlit run ' + filename + ' ' + ' '.join(args))
+
+@main.command("predict")
+@click.argument('project_path', type = click.Path(exists=True)
+                , required=True)
+@click.argument('pose_files', type = list, required=True)
+@click.option("-origin", "pose_origin", type = str, default = None
+              , help="origin of the pose files, if not specified, the origin from the config file is used")
+@click.option("-verbose", "verbose", is_flag=True, default=False, help="verbose output")
+def main_predict(project_path, pose_files, pose_origin, verbose):
+    """Uses A-SOiD classifier to predict on new pose files.
+
+    :param project_path: path to project containing config file
+
+    :param pose_files: list of paths to pose files, or path to folder with pose files, or path to single pose file
+
+    :param pose_origin: origin of the pose files, if not specified, the origin from the config file is used
+
+    :param verbose: verbose output
+
+    :output: the predictions are saved as .csv files in the same directory as the pose files"""
+    click.echo("Predicting on new pose files...")
+    asoid_project = Project(project_path, verbose=verbose)
+    _ = asoid_project.predict(pose_files, pose_origin)
+
 
 if __name__ == "__main__":
     main()
