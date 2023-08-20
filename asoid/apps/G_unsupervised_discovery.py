352fe1df45d08c31f3f28fb40f92fd9ab73777bd
Alex <40252919+runninghsus@users.noreply.github.com>
2023-08-20 15:43:45 -0400

    Update G_unsupervised_discovery.py

--- asoid/apps/G_unsupervised_discovery.py
+++ asoid/apps/G_unsupervised_discovery.py
@@ -30,48 +30,41 @@ TITLE = "Unsupervised discovery"
 
 
 def prompt_setup(software, ftype, selected_bodyparts, annotation_classes,
-                 framerate, videos_dir, project_dir, iter_dir, pose_expander):
-    # pose_exapnder = st.expander('pose'.upper(), expanded=True)
+                 framerate, videos_dir, project_dir, iter_dir, pose_expander, left_checkbox):
+
     if software == 'CALMS21 (PAPER)':
         ROOT = Path(__file__).parent.parent.parent.resolve()
         new_pose_sav = os.path.join(ROOT.joinpath("new_test"), './new_pose.sav')
         new_pose_list = load_new_pose(new_pose_sav)
     else:
-
-        new_pose_csvs = pose_expander.file_uploader('Upload Corresponding Pose Files',
-                                                    accept_multiple_files=True,
-                                                    type=ftype, key='pose')
-
-        # st.write(new_pose_csvs, new_videos)
-        if new_pose_csvs is not None:
-            new_pose_list = []
-            for i, f in enumerate(new_pose_csvs):
-                current_pose = pd.read_csv(f,
-                                           header=[0, 1, 2], sep=",", index_col=0)
-                bp_level = 1
-                bp_index_list = []
-                for bp in selected_bodyparts:
-                    bp_index = np.argwhere(current_pose.columns.get_level_values(bp_level) == bp)
-                    bp_index_list.append(bp_index)
-
-                selected_pose_idx = np.sort(np.array(bp_index_list).flatten())
-
-                # get rid of likelihood columns for deeplabcut
-                idx_llh = selected_pose_idx[2::3]
-                # the loaded sleap file has them too, so exclude for both
-                idx_selected = [i for i in selected_pose_idx if i not in idx_llh]
-                #
-                # idx_selected = np.array([0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16])
-                new_pose_list.append(np.array(current_pose.iloc[:, idx_selected]))
-
-            st.session_state['uploaded_pose'] = new_pose_list
-            # col1, col3 = st.columns(2)
+        if left_checkbox:
+            new_pose_csvs = pose_expander.file_uploader('Upload Corresponding Pose Files',
+                                                        accept_multiple_files=True,
+                                                        type=ftype, key='pose')
 
 
-
+            if len(new_pose_csvs) > 0:
+                new_pose_list = []
+                for i, f in enumerate(new_pose_csvs):
+                    current_pose = pd.read_csv(f,
+                                               header=[0, 1, 2], sep=",", index_col=0)
+                    bp_level = 1
+                    bp_index_list = []
+                    for bp in selected_bodyparts:
+                        bp_index = np.argwhere(current_pose.columns.get_level_values(bp_level) == bp)
+                        bp_index_list.append(bp_index)
+                    selected_pose_idx = np.sort(np.array(bp_index_list).flatten())
+                    # get rid of likelihood columns for deeplabcut
+                    idx_llh = selected_pose_idx[2::3]
+                    # the loaded sleap file has them too, so exclude for both
+                    idx_selected = [i for i in selected_pose_idx if i not in idx_llh]
+                    # idx_selected = np.array([0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16])
+                    new_pose_list.append(np.array(current_pose.iloc[:, idx_selected]))
+                st.session_state['uploaded_pose'] = new_pose_list
+            else:
+                st.session_state['uploaded_pose'] = []
         else:
             st.session_state['uploaded_pose'] = []
-            # st.session_state['uploaded_vid'] = None
 
 
 def get_features_labels(X, y, iterX_model, frames2integ, project_dir, iter_folder, placeholder,
@@ -79,29 +72,34 @@ def get_features_labels(X, y, iterX_model, frames2integ, project_dir, iter_folde
     features = [None]
     predict_arr = [None]
     processed_input_data = st.session_state['uploaded_pose']
-
+    old_feats = X.copy()
+    old_labels = y.copy()
     if placeholder.button('preprocess files'):
-        st.session_state['disabled'] = True
-        # extract features, bin them
-        features = []
-        for i, data in enumerate(processed_input_data):
-            # using feature scaling from training set
-            feats, _ = feature_extraction([data], 1, frames2integ)
-            features.append(feats)
-        predict_arr = []
-        for i in stqdm(range(len(features)), desc="Behavior prediction from spatiotemporal features"):
-            with st.spinner('Predicting behavior from features...'):
-                predict = bsoid_predict_numba_noscale([features[i]], iterX_model)
-                pred_proba = bsoid_predict_proba_numba_noscale([features[i]], iterX_model)
-                predict_arr.append(np.array(predict).flatten())
-        new_feats = np.vstack(features)
-        old_feats = X.copy()
-        new_labels = np.hstack(predict_arr)
-        old_labels = y.copy()
-        # st.write(np.vstack((old_feats, new_feats)).shape, np.hstack((old_labels, new_labels)).shape)
+        if len(processed_input_data) > 0:
+            st.session_state['disabled'] = True
+            # extract features, bin them
+            features = []
+            for i, data in enumerate(processed_input_data):
+                # using feature scaling from training set
+                feats, _ = feature_extraction([data], 1, frames2integ)
+                features.append(feats)
+            predict_arr = []
+            for i in stqdm(range(len(features)), desc="Behavior prediction from spatiotemporal features"):
+                with st.spinner('Predicting behavior from features...'):
+                    predict = bsoid_predict_numba_noscale([features[i]], iterX_model)
+                    pred_proba = bsoid_predict_proba_numba_noscale([features[i]], iterX_model)
+                    predict_arr.append(np.array(predict).flatten())
+            new_feats = np.vstack(features)
+            new_labels = np.hstack(predict_arr)
+            all_feats = np.vstack((old_feats, new_feats))
+            all_labels = np.hstack((old_labels, new_labels))
+        else:
+            all_feats = old_feats
+            all_labels = old_labels
+
         with st.spinner('Saving...'):
             save_data(project_dir, iter_folder, 'embedding_input.sav',
-                  [np.vstack((old_feats, new_feats)), np.hstack((old_labels, new_labels))])
+                  [all_feats, all_labels])
         st.session_state['input_sav'] = os.path.join(project_dir, iter_folder, 'embedding_input.sav')
         st.success('Done. Type "R" to Refresh.')
 
@@ -145,8 +143,6 @@ def pca_umap_hdbscan(target_behavior, annotation_classes, input_sav, cluster_ran
         for target_behav in target_behavior:
             target_beh_id = annotation_classes.index(target_behav)
             selected_features = features[predictions==target_beh_id]
-            st.write(selected_features.shape)
-
             scalar = StandardScaler()
             selected_feats_scaled = scalar.fit_transform(selected_features)
             pca = PCA(random_state=42)
@@ -166,29 +162,6 @@ def pca_umap_hdbscan(target_behavior, annotation_classes, input_sav, cluster_ran
             st.success('Done. Type "R" to Refresh.')
 
 
-def plot_targeted_discovery(target_behavior, output_sav):
-    with open(output_sav, 'rb') as fr:
-        [umap_embeddings, assignments, soft_assignments] = joblib.load(fr)
-
-    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
-
-    colors = sns.color_palette('husl', n_colors=len(np.unique(assignments)))
-    for i, g in enumerate(np.unique(soft_assignments)):
-        # if g >= 0:
-
-        idx_g = np.where(soft_assignments == g)[0]
-        ax.scatter(umap_embeddings[idx_g, 0], umap_embeddings[idx_g, 1],
-                   s=0.5,
-                   c=colors[i]
-                   # cmap=plt.cm.Spectral,
-                   # label=[f'groom sub{i}' for i in range(len(np.unique(assignments)))]
-                   )
-
-    ax.set_title(f'{target_behavior[0]} subtypes')
-    # ax.legend([f'groom sub{i}' for i in range(len(np.unique(assignments)))])
-    st.pyplot(fig)
-
-
 def plot_hdbscan_embedding(output_sav):
     with open(output_sav, 'rb') as fr:
         [umap_embeddings, assignments, soft_assignments] = joblib.load(fr)
@@ -201,7 +174,6 @@ def plot_hdbscan_embedding(output_sav):
     trace_list = []
 
     for num,g in enumerate(unique_classes):
-
         if g < 0:
             idx = np.where(assignments == g)[0]
             trace_list.append(go.Scatter(x=umap_embeddings[idx,0],
@@ -212,12 +184,10 @@ def plot_hdbscan_embedding(output_sav):
                               )
         else:
             idx = np.where(assignments == g)[0]
-
             trace_list.append(go.Scatter(x=umap_embeddings[idx,0],
                                          y=umap_embeddings[idx,1],
                                          name=group_types[num],
                                          mode='markers'
-
                                          ))
 
     fig = make_subplots()
@@ -229,7 +199,7 @@ def plot_hdbscan_embedding(output_sav):
     fig.update_layout(title_text="Unsupervised Clustering",
                       )
 
-    return fig,group_types
+    return fig, group_types
 
 
 def main(ri=None, config=None):
@@ -264,9 +234,11 @@ def main(ri=None, config=None):
 
         st.session_state['disabled'] = False
         left_col, right_col = st.columns(2)
+        left_checkbox = left_col.checkbox('Add Additional Pose Files?')
+
         pose_expander = left_col.expander('pose'.upper(), expanded=True)
         prompt_setup(software, ftype, selected_bodyparts, annotation_classes,
-                     framerate, videos_dir, project_dir, iter_folder, pose_expander)
+                     framerate, videos_dir, project_dir, iter_folder, pose_expander, left_checkbox)
         [features, targets, _, frames2integ] = load_features(project_dir, iter_folder)
         if features.shape[0] > targets.shape[0]:
             X = features[:targets.shape[0]].copy()
@@ -296,7 +268,6 @@ def main(ri=None, config=None):
                 st.session_state['input_sav'] = None
                 st.success('Cleared. Type "R" to Refresh.')
             if st.session_state['output_sav'] is None:
-
                 pca_umap_hdbscan(target_behavior, annotation_classes, st.session_state['input_sav'], [5, 5.5],
                                  project_dir, iter_folder, buttonL)
 
@@ -304,12 +275,9 @@ def main(ri=None, config=None):
                 if buttonR.button(':red[Clear Embedding.]'):
                     st.session_state['output_sav'] = None
                     st.success('Cleared. Type "R" to Refresh.')
-                # plot_targeted_discovery(target_behavior, st.session_state['output_sav'])
                 fig, group_types = plot_hdbscan_embedding(st.session_state['output_sav'])
-                buttonL.plotly_chart(fig)
-        # st.subheader("Directed discovery")
-        # explorer = Explorer(config)
-        # explorer.main()
+                buttonL.plotly_chart(fig, use_container_width=True)
+
 
     else:
         st.error(NO_CONFIG_HELP)
